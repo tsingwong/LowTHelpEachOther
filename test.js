ac086263a6b01f4b8315731a3ffc1430b4a1b2b3
tsingwong <919916459@qq.com>
2020-04-15 12:01:48 +0800

    fix: rm test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-typeof undefined === 'undefined' // true
-typeof true === 'boolean' // true
-typeof 42 === 'number' // true
-typeof '42' === 'string' // true
-typeof { life: 42 } === 'object' // true
-// ES6中新加入的类型
-typeof Symbol() === 'symbol' // true
